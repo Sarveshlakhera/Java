b77f36fa2af0aff869f961e93be1bebd59e8dfa6
vinay.sharma <vinay.sharma@jitterbit.com>
2023-08-15 12:10:14 +0530

    Implemented pattern logging using sl4j/log4j

---
+++ logging4j/logging4j/src/main/java/com/logging/log4j/Sl4jLogging.java
@@ -0,0 +1,28 @@
+/** Copyright © 2022 Jitterbit, Inc.
+ *
+ * Licensed under the JITTERBIT MASTER SUBSCRIPTION AGREEMENT
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * https://www.jitterbit.com/cloud-eula
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.logging.log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author sarvesh.lakhera
+ *
+ */
+public class Sl4jLogging {
+  private static Logger logger = LoggerFactory.getLogger(Sl4jLogging.class);
+
+  public static void main(String[] args) {
+    logger.info("Hello");
+  }
+}
---
+++ logging4j/src/main/java/com/logging/log4j/EnvIdPattern.java
@@ -0,0 +1,49 @@
+/** Copyright © 2022 Jitterbit, Inc.
+ *
+ * Licensed under the JITTERBIT MASTER SUBSCRIPTION AGREEMENT
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * https://www.jitterbit.com/cloud-eula
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.logging.log4j;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.pattern.ConverterKeys;
+import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
+
+/**
+ * @author vinay.sharma
+ *
+ */
+@Plugin(name = "EnvIdPattern", category = LogEventPatternConverter.CATEGORY)
+@ConverterKeys({"envId"})
+public class EnvIdPattern extends LogEventPatternConverter {
+
+  /**
+   * @param name
+   * @param style
+   */
+  protected EnvIdPattern(String[] options) {
+    super("EnvIdPattern", null);
+  }
+  
+  /**
+   * Obtains an instance of pattern converter.
+   *
+   * @param options options, may be null.
+   * @return instance of pattern converter.
+   */
+  public static EnvIdPattern newInstance(final String[] options) {
+      return new EnvIdPattern(options);
+  }
+
+  @Override
+  public void format(LogEvent event, StringBuilder toAppendTo) {
+    toAppendTo.append("23423");
+  }
+}
