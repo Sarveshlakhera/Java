ef5d01b13df886d802fd09507f01d205054963f7
Sarveshlakhera <sarveshlakhera@gmail.com>
2025-08-03 22:43:14 +0530

    Added home JSP

---
+++ spring-boot-demo/spring-boot-demo/src/test/java/com/java/spring_boot_demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
